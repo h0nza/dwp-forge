3ce5d288eb28e2e0d80f0a2e663fe11b8ffe14f3
ostrovsm <ostrovsm@709dd3f2-9485-11dd-9c30-33875a484c2d>
2008-10-07 15:59:11 +0000

    Release of 2008-09-29
     * Added Spanish localization
     * Added Russian localization
    
    git-svn-id: https://dwp-forge.googlecode.com/svn/trunk@4 709dd3f2-9485-11dd-9c30-33875a484c2d

---
+++ columns/lang/es/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Digna González Otero <digna.gonzalezotero [at] gmail [dot] com>
+ */
+
+$lang['kwcolumns'] = 'columnas';
+$lang['kwnewcol'] =  'nueva_columna';
+
---
+++ columns/lang/ru/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Russian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Mykola Ostrovskyy <spambox03@mail.ru>
+ */
+
+$lang['kwcolumns'] = 'колонки';
+$lang['kwnewcol'] =  'новаяколонка';
+
